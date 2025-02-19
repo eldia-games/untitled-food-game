538f43fb200569d3298f893b134f53913f35ab46
David Rodriguez Rives   -   The Pirate Cat <miniesda@gmail.com>
2025-02-19 19:48:38 +0100

    MakePlayerCombat (without attack functionality)

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -8,18 +8,19 @@ public class PlayerCombat : MonoBehaviour
 
     public Animator _anim;
     private float _translationSpeed = 5f;
-    private float _runningSpeed = 10f;
-
     private float cooldownAttack = 1.01f;
-    private float cooldownSlide = 1f;
+    private float cooldownSlide = 1.0f;
     private InputHandler _handler;
     private int moving;
 
     private bool attackAvailable = true;
-    private bool slideAvailable = true;
-    public new Camera camera;
 
+    [Range(0,1)]
+    public float slideAvailable = 1;
+    private bool slideForce = false;
+    public new Camera camera;
     public GameObject player;
+    private Vector3 lookAtPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +31,7 @@ public class PlayerCombat : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
         //print(_handler.input);
@@ -41,40 +42,55 @@ public class PlayerCombat : MonoBehaviour
         else 
             moving = 0;
         _anim.SetFloat("Moving", moving);
-        
+
         transform.Translate(Vector3.forward * (_handler.input.y * Time.deltaTime * _translationSpeed));
         transform.Translate(Vector3.right * (_handler.input.x * Time.deltaTime * _translationSpeed));
 
         //player rotate to wasd
         if(attackAvailable)
         {
-        player.transform.LookAt(player.transform.position + new Vector3(_handler.input.x, 0, _handler.input.y));
+        lookAtPosition = player.transform.position + new Vector3(_handler.input.x, 0, _handler.input.y);
+        player.transform.LookAt(lookAtPosition);
         player.transform.eulerAngles = new Vector3(0, player.transform.eulerAngles.y, 0);
         }
 
 
         //actions
-        if(_handler.attack)
-        {
-            
-            Vector3 mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
-            //print(mousePosition);
-            player.transform.LookAt(mousePosition);
-            player.transform.eulerAngles = new Vector3(0, player.transform.eulerAngles.y, 0);
-        
-            if(attackAvailable)
-            {
-                attackAvailable = false;
-                _anim.SetTrigger("Attack");
-            
-                StartCoroutine(AttackCooldown());
-            }
-        }
-        if(_handler.slide && slideAvailable)
+        //slide
+        if(_handler.slide && slideAvailable==1)
         {
             _anim.SetTrigger("Slide");
-            slideAvailable = false;
+            slideAvailable = 0;
+            slideForce = true;
+            StartCoroutine(SlideForce());
+            //addforce to dodge
             StartCoroutine(SlideCooldown());
+        }
+        
+        //attack
+        if(_handler.attack)
+                {
+                    
+                    Vector3 mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
+                    //print(mousePosition);
+                    player.transform.LookAt(mousePosition);
+                    player.transform.eulerAngles = new Vector3(0, player.transform.eulerAngles.y, 0);
+                
+                    if(attackAvailable)
+                    {
+                        attackAvailable = false;
+                        _anim.SetTrigger("Attack");
+                    
+                        StartCoroutine(AttackCooldown());
+                    }
+                }
+
+        //Make the slide movement
+        if(slideForce)
+        {
+            //print("sliding");
+            transform.Translate(Vector3.forward * (_handler.input.y * Time.deltaTime * _translationSpeed) * 2f);
+            transform.Translate(Vector3.right * (_handler.input.x * Time.deltaTime * _translationSpeed) * 2f);
         }
     }
 
@@ -83,10 +99,20 @@ public class PlayerCombat : MonoBehaviour
         yield return new WaitForSeconds(cooldownAttack);
         attackAvailable = true;
     }
-
     IEnumerator SlideCooldown()
     {
-        yield return new WaitForSeconds(cooldownSlide);
-        slideAvailable = true;
+        float elapsedTime = 0f;
+        while (elapsedTime < cooldownSlide)
+        {
+            slideAvailable = elapsedTime / cooldownSlide;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        slideAvailable = 1;
+    }
+    IEnumerator SlideForce()
+    {
+        yield return new WaitForSeconds(0.2f);
+        slideForce = false;
     }
 }
