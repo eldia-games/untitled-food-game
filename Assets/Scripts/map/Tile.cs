d5847b74e1cbbdbbbc46953e1b2c9e2203b21a23
Ediu3095 <eduardoruizsabajanes@gmail.com>
2025-03-02 11:36:08 +0100

    New map generation algorithm

--- Assets/Scripts/map/Tile.cs
+++ Assets/Scripts/map/Tile.cs
@@ -2,56 +2,73 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using Random = UnityEngine.Random;
 
 public class Tile {
   private TileData tileData_;
   private Vector3 position_;
-  private int type_;
+  private int roads_;
+  private TileType type_;
+  private RoomType room_;
 
-  public Tile(TileData tileData, Vector3 position, int type) {
+  public Tile(TileData tileData, Vector3 position, TileType type) {
     tileData_ = tileData;
     position_ = position;
-    type_ = type;
+    roads_    = 0;
+    type_     = type;
+    room_     = 0;
   }
 
   public void Connect(Vector2Int offset) {
-    if (offset.x < 0) type_ |= 0b0001;  // Path from top
-    if (offset.y < 0) type_ |= 0b0010;  // Path from bottom
-    if (offset.y > 0) type_ |= 0b0100;  // Path to top
-    if (offset.x > 0) type_ |= 0b1000;  // Path to bottom
-  }
-
-  public bool HasRoad() {
-    return (type_ & 0b1111) != 0;
+    if (offset.x < 0) roads_ |= 0b0001;  // Path from top
+    if (offset.y < 0) roads_ |= 0b0010;  // Path from bottom
+    if (offset.y > 0) roads_ |= 0b0100;  // Path to top
+    if (offset.x > 0) roads_ |= 0b1000;  // Path to bottom
   }
 
   public bool HasRoad(Vector2Int offset) {
     int road = 0;
-    if (offset.x < 0) road |= type_ & 0b0001;  // Path from top
-    if (offset.y < 0) road |= type_ & 0b0010;  // Path from bottom
-    if (offset.y > 0) road |= type_ & 0b0100;  // Path to top
-    if (offset.x > 0) road |= type_ & 0b1000;  // Path to bottom
+    if (offset.x < 0) road |= roads_ & 0b0001;  // Path from top
+    if (offset.y < 0) road |= roads_ & 0b0010;  // Path from bottom
+    if (offset.y > 0) road |= roads_ & 0b0100;  // Path to top
+    if (offset.x > 0) road |= roads_ & 0b1000;  // Path to bottom
     return road != 0;
   }
 
-  public bool IsRiver() {
-    return (type_ & 0b10000) != 0;
+  public bool HasRoom() {
+    return type_ == TileType.Room && roads_ != 0;
+  }
+
+  public void SetRoom(RoomType type) {
+    Assert.IsTrue(type_ == TileType.Room);
+    room_ = type;
   }
 
   public GameObject Instantiate(Transform parent) {
-    int index = type_;
-    // if (type_ == 0b10000) index = 0b10000;
-    if (type_ == 0b11001) index = 0b10001;
-    if (type_ == 0b10110) index = 0b10010;
+    int index = roads_;
+    if (type_ == TileType.Fence || type_ == TileType.River || type_ == TileType.Wall) {
+      if (roads_ == 0b0000) index = 0;
+      if (roads_ == 0b1001) index = 1;
+      if (roads_ == 0b0110) index = 2;
+    }
 
-    GameObject instance = tileData_.lookup[index];
+    GameObject instance;
+    switch (type_) {
+      case TileType.Fence: instance = tileData_.fences[index]; break;
+      case TileType.River: instance = tileData_.rivers[index]; break;
+      case TileType.Wall:  instance = tileData_.walls [index]; break;
+      default:             instance = tileData_.roads [index]; break;
+    }
     instance = GameObject.Instantiate(instance, position_, instance.transform.rotation, parent);
-    return instance;
-  }
 
-  public override string ToString() {
-    string value = Convert.ToString(type_, 2);
-    while (value.Length < 5) value = "0" + value;
-    return "0b" + value;
+    if (type_ == TileType.Room && roads_ != 0) {
+      GameObject decoration = tileData_.rooms[(int) room_];
+      Vector3 pos = instance.transform.position + Vector3.forward * 2;
+      Quaternion rot = decoration.transform.rotation;
+      decoration = GameObject.Instantiate(decoration, pos, rot, instance.transform);
+    }
+
+    return instance;
   }
 }
