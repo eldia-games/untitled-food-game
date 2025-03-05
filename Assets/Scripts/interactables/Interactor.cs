7fe2100de4ee2125e0fdca1647d4d7c206ba3e64
ikervic14 <ikervic14@gmail.com>
2025-03-05 14:29:44 +0100

    Personaje añadido a la mazmorra y añadida la interacion con los objetos

--- Assets/Scripts/interactables/Interactor.cs
+++ Assets/Scripts/interactables/Interactor.cs
@@ -16,10 +16,12 @@ public class Interactor : MonoBehaviour
     public void EnterInteractableRange(Interactable interact)
     {
         interactable = interact;
+        Debug.Log("can interact");
     }
     public void ExitUnteractableRange()
     {
         interactable = null;
+        Debug.Log("cant interact");
     }
     public void interact()
     {
