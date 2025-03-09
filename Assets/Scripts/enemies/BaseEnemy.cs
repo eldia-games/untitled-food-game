c697d44efd812396a8ec9aefc9e2d69a120bdb6e
lmcomeche <luismicsandres@hotmail.com>
2025-03-09 16:59:02 +0100

    Arreglada visibilidad al jugador

--- Assets/Scripts/enemies/BaseEnemy.cs
+++ Assets/Scripts/enemies/BaseEnemy.cs
@@ -122,12 +122,12 @@ public abstract class BaseEnemy : MonoBehaviour
     protected virtual void CheckPlayerVisibility()
     {
         if(player == null) return; // Por seguridad
-        float distance = Vector3.Distance(transform.position, player.transform.position);
-        float angle = Vector3.Angle(transform.forward, player.transform.position - transform.position);
+        Vector3 origin = transform.position + Vector3.up * 1.5f;
+        float distance = Vector3.Distance(origin, player.transform.position);
+        float angle = Vector3.Angle(transform.forward, player.transform.position - origin);
 
         // Hacemos un raycast para verificar que no hay obstáculos
         RaycastHit hit;
-        Vector3 origin = transform.position + Vector3.up * 1.5f;
         bool isHit = Physics.Raycast(origin, player.transform.position - origin, out hit, distance);
 
         // Se ve si: 
@@ -203,9 +203,9 @@ public abstract class BaseEnemy : MonoBehaviour
     protected IEnumerator WaitAndMove()
     {
         isWaitAndMove = true;
-        yield return new WaitForSeconds(Random.Range(1, 3));
+        yield return new WaitForSeconds(Random.Range(1, 6));
         // Mover a una posición aleatoria dentro de un radio
-        agent.SetDestination(initialPosition + Random.insideUnitSphere * 3f);
+        agent.SetDestination(initialPosition + Random.insideUnitSphere * 4f);
         isWaitAndMove = false;
     }
 
