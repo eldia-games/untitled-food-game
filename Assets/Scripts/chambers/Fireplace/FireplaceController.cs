5a315e28f0277e5aaef24305289b75057534b400
ikervic14 <ikervic14@gmail.com>
2025-03-09 20:36:02 +0100

    finalizadas la sala de tesoro y de hoguera

--- Assets/Scripts/chambers/Fireplace/FireplaceController.cs
+++ Assets/Scripts/chambers/Fireplace/FireplaceController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,14 @@ public class FireplaceController : MonoBehaviour, IChamberController
 {
 
     [SerializeField] private GameObject player;
+    [SerializeField] private CinemachineDollyCart cart;
+    [SerializeField] private GameObject exit;
+    [SerializeField] private GameObject fire;
+    [SerializeField] private Animator playerAnimator;
 
     private bool healed = false;
     private int healLevel=0;
+
 
 
 
@@ -17,24 +23,47 @@ public class FireplaceController : MonoBehaviour, IChamberController
     {
         healLevel = 7 * level;
     }
-    public void Start()
-    {
-        player.GetComponent<PlayerCombat>().enabled = true;
-    }
+
 
     public void Heal(GameObject fireplace)
     {
         if (!healed)
         {
             healed = true;
-            //healed.GetComponent<Animator>().SetBool("On", false); //apagar la hoguera
-            //curar al jugador
+            fire.SetActive(false);
             PlayerStatsController.Instance.healprct(healLevel);
             fireplace.GetComponent<Interactable>().Desactive();
 
         }
     }
+    public void StartMovingCart()
+    {
+        cart.m_Position = 0;
+        cart.m_Speed = 2;
+    }
+    public void OnExit()
+    {
+        GameManager.Instance.EnterMapScene();
+    }
+    public void StartDungeonEnterAnimation()
+    {
+        playerAnimator.SetFloat("Moving", 1);
+        StartCoroutine(EnterDungeon());
+    }
 
+    IEnumerator EnterDungeon()
+    {
+        for (int i = 0; i < 1.5f / Time.fixedDeltaTime; i++)
+        {
+            player.transform.Translate(Vector3.forward * Time.fixedDeltaTime*1.5f);
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+        }
+        playerAnimator.SetFloat("Moving", 0);
+        yield return new WaitForSeconds(0.5f);
+        exit.SetActive(true);
+        yield return new WaitForSeconds(1);
+        player.GetComponent<PlayerCombat>().enabled = true;
 
+    }
 
 }
