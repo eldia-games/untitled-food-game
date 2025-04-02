a18b364b1d0ac4a5cdd30e866ee253cc37ed15e6
lmcomeche <luismicsandres@hotmail.com>
2025-04-02 17:44:40 +0200

    Bosses
    Se introducen los enemigos jefe

---
+++ Assets/Scripts/Enemies/BossSkills/BossSkillScriptableObject.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class BossSkillScriptableObject : ScriptableObject
+{
+    public float cooldown = 10f;
+    public int damage = 5;
+    public float minRange = 5.0f;
+    public float maxRange = 10.0f;
+
+    public bool isCasting = false;
+
+    protected float castTime = 0.0f;
+
+    public virtual void Initialize()
+    {
+        castTime = Time.time;
+        isCasting = false;
+    } 
+
+    public virtual void Use(Boss boss, GameObject player)
+    {
+        isCasting = true;
+    }
+
+    public virtual bool CanUse(Boss boss, GameObject player)
+    {
+        return true;
+    }
+
+    public virtual bool InMinRange(Boss boss, GameObject player)
+    {
+        return true;
+    }
+
+    public virtual void OnAnimationEvent(Boss boss, GameObject player)
+    {
+        return;
+    }
+}
