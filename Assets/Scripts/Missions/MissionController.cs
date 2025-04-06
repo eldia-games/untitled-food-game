376e25c7a0032371f422153e06f23926ae5d7485
ikervic14-git <ikervic14@gmail.com>
2025-04-06 14:44:07 +0200

    cambios misiones

--- Assets/Scripts/Missions/MissionController.cs
+++ Assets/Scripts/Missions/MissionController.cs
@@ -20,21 +20,24 @@ public class MissionController : MonoBehaviour
     public List<Mission> GetMissions()
     {
         Debug.Log("holaaaa");
-        if (inventory != null)
+        if (inventory == null)
         {
-            if(inventory.getMissions().Count == 0)
-            {
-                List<Mission> missions = new List<Mission>();
-                for (int i = 0; i < numMissions; i++)
-                {
-                    missions.Add(GenerateMission(i));
+            inventory = InventorySafeController.Instance;
 
-                }
-                inventory.setsMissions(missions);
-            }
-            return inventory.getMissions();
         }
-        return null;
+
+        if(inventory.getMissions().Count == 0)
+        {
+            List<Mission> missions = new List<Mission>();
+            for (int i = 0; i < numMissions; i++)
+            {
+                missions.Add(GenerateMission(i));
+
+            }
+            inventory.setsMissions(missions);
+        }
+        return inventory.getMissions();
+
     }
     public Mission GenerateMission(int index)
     {
