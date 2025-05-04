5785a668448889e3566c0c02017e119f44d181e1
alopezmora2017 <61735675+alopezmora2017@users.noreply.github.com>
2025-05-04 22:13:03 +0200

    menu tutorial

--- Assets/Scripts/Gamemanager/GameManager.cs
+++ Assets/Scripts/Gamemanager/GameManager.cs
@@ -5,63 +5,73 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-  #region MonoBehaviour
+    #region MonoBehaviour
 
-  public static GameManager Instance { get; private set; }
+    public static GameManager Instance { get; private set; }
 
-  public PlayerStats playerStats;
+    public PlayerStats playerStats;
 
-  private void Awake() {
-    if (Instance != null && Instance != this) {
-      Destroy(gameObject);
-    } else {
-      Instance = this;
-      DontDestroyOnLoad(gameObject);
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
-  }
 
-  #endregion
+    #endregion
 
-  #region map
+    #region map
 
-  private int mapSize_;
-  private List<Tile> map_;
+    private int mapSize_;
+    private List<Tile> map_;
 
-  public List<Tile> map {
-    get { return map_; }
-    set {
-      Assert.IsNull(map_);  // Do NOT overwrite maps...
-      map_ = value;
-      mapSize_ = (int)Mathf.Sqrt(map_.Count);
+    public List<Tile> map
+    {
+        get { return map_; }
+        set
+        {
+            Assert.IsNull(map_);  // Do NOT overwrite maps...
+            map_ = value;
+            mapSize_ = (int)Mathf.Sqrt(map_.Count);
+        }
     }
-  }
 
-  public void ClearMap() {
-    map_ = null;
-    mapSize_ = 0;
-  }
+    public void ClearMap()
+    {
+        map_ = null;
+        mapSize_ = 0;
+    }
 
-  private void ClearPlayerStats() {
-    playerStats.Reset();
-  }
+    private void ClearPlayerStats()
+    {
+        playerStats.Reset();
+    }
 
-  public Vector2Int tile { get; set; }
+    public Vector2Int tile { get; set; }
 
-  public RoomType room { get { return map[tile.x * mapSize_ + tile.y].GetRoom(); } }
+    public RoomType room { get { return map[tile.x * mapSize_ + tile.y].GetRoom(); } }
 
-  #endregion
+    #endregion
 
-  #region SceneManagement
+    #region SceneManagement
 
-  public void EnterMainMenuScene() {
-    Time.timeScale = 1;
-    AudioManager.Instance.PlayMenuMusic();
-    UIManager.Instance.ShowMainMenuCanvas();
-    ClearMap();
-    SceneManager.LoadScene("MainMenu");
-  }
+    public void EnterMainMenuScene()
+    {
+        Time.timeScale = 1;
+        AudioManager.Instance.PlayMenuMusic();
+        UIManager.Instance.ShowMainMenuCanvas();
+        ClearMap();
+        SceneManager.LoadScene("MainMenu");
+    }
 
   public void EnterLobbyScene() {
     Time.timeScale = 1;
@@ -73,18 +83,20 @@ public class GameManager : MonoBehaviour {
     SceneManager.LoadScene("Lobby");
   }
 
-  public void NewGame() {
-    InventorySafeController.Instance.newGame();
-    PowerUpStatsController.Instance.newGame();
-    EnterLobbyScene();
-  }
+    public void EnterNewGameScene()
+    {
+        SceneManager.LoadScene("Basement");
+        AudioManager.Instance.PlayLobbyMusic();
+        UIManager.Instance.ShowBasementCanvas();
+        UIManager.Instance.ShowWelcomeCanvas();
+    }
 
-  public void ContinueGame() {
-    InventorySafeController.Instance.loadGame();
-    PowerUpStatsController.Instance.loadGame();
-    EnterLobbyScene();
-    //a�adir persistencia
-  }
+    public void NewGame()
+    {
+        InventorySafeController.Instance.newGame();
+        PowerUpStatsController.Instance.newGame();
+        EnterNewGameScene();
+    }
 
   public void VictoryReturn() {
     //a�adir persistencia guardar loot en inventario taberna
@@ -94,26 +106,36 @@ public class GameManager : MonoBehaviour {
     //a�adir persistencia
   }
 
-  public void EnterMapScene() {
-    SceneManager.LoadScene("Map");
-    AudioManager.Instance.PlayMapMusic();
-    UIManager.Instance.ShowMapCanvas();
-  }
+    public void ContinueGame()
+    {
+        InventorySafeController.Instance.loadGame();
+        PowerUpStatsController.Instance.loadGame();
+        EnterLobbyScene();
+        //a�adir persistencia
+    }
 
-  public void EnterChamberScene() {
-    AudioManager.Instance.PlayChamberMusic();
-    UIManager.Instance.ShowChamberCanvas();
-    UIManager.Instance.ShowChamberNamePopUpCanvas(room);
-    SceneManager.LoadScene("Chamber");
-  }
+    public void EnterMapScene()
+    {
+        SceneManager.LoadScene("Map");
+        AudioManager.Instance.PlayMapMusic();
+        UIManager.Instance.ShowMapCanvas();
+    }
 
-  public void EnterTrainingScene()
-  {
-    AudioManager.Instance.PlayLobbyMusic();
-    UIManager.Instance.HidePopUpCanvas();
-    UIManager.Instance.HideAllCanvas();
-    UIManager.Instance.ShowBasementCanvas();
-    SceneManager.LoadScene("Basement");
+    public void EnterChamberScene()
+    {
+        AudioManager.Instance.PlayChamberMusic();
+        UIManager.Instance.ShowChamberCanvas();
+        UIManager.Instance.ShowChamberNamePopUpCanvas(room);
+        SceneManager.LoadScene("Chamber");
+    }
+
+    public void EnterTrainingScene()
+    {
+        AudioManager.Instance.PlayLobbyMusic();
+        UIManager.Instance.HidePopUpCanvas();
+        UIManager.Instance.HideAllCanvas();
+        UIManager.Instance.ShowBasementCanvas();
+        SceneManager.LoadScene("Basement");
     }
 
     #endregion
@@ -121,13 +143,15 @@ public class GameManager : MonoBehaviour {
     #region WeaponType
 
     public int _weaponType = 0;
-  public int getCurrentWeaponType() {
-    return _weaponType;
-  }
+    public int getCurrentWeaponType()
+    {
+        return _weaponType;
+    }
 
-  public void setCurrentWeaponType(int weaponType) {
-    _weaponType = weaponType;
-    playerStats.SetWeaponType(weaponType);
-  }
-  #endregion
+    public void setCurrentWeaponType(int weaponType)
+    {
+        _weaponType = weaponType;
+        playerStats.SetWeaponType(weaponType);
+    }
+    #endregion
 }
